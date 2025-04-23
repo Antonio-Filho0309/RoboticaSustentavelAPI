5072a1f4ebb8a696e1ed6608d76c91e5930ed3b1
Antonio Filho <antoniodollmichael@gmail.com>
2025-04-22 22:42:02 -0300

    Creating a generic class for service responses

--- RoboticaSustentavelAPI/Program.cs
+++ RoboticaSustentavelAPI/Program.cs
@@ -12,8 +12,6 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
