c6be525bdfb30fcef0e77dfa55ebc79b36167070
Antonio Filho <antoniodollmichael@gmail.com>
2025-05-12 22:26:33 -0300

    Created Container

--- RoboticaSustentavelAPI/Program.cs
+++ RoboticaSustentavelAPI/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Pipeline
+builder.WebHost.UseUrls("http://0.0.0.0:80");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseAuthorization();
